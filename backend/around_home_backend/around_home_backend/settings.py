6b72f97366ff677764794d61ff5ce294905b4456
Lynn Chien <moneychien.aom@gmail.com>
2022-03-09 03:33:40 +0800

    migrate DB to AWS RDS

--- backend/around_home_backend/around_home_backend/settings.py
+++ backend/around_home_backend/around_home_backend/settings.py
@@ -82,9 +82,14 @@ WSGI_APPLICATION = 'around_home_backend.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "around_home_db",
+        "USER": "postgres",
+        "PASSWORD": "JXYg8pMSlYq0oxsu4uPO",
+        "HOST": "around-home-db.cnvztdco9aoh.us-east-1.rds.amazonaws.com",
+        "PORT": "5432"
+    }
 }
 
 # Password validation
